949772db7d5fe532bf73390deaa999172a7b5e6a
Jordi Adell <jadell@verbio.com>
2017-02-10 09:53:38 +0100

    fixed include and lib dirs to follow standards

--- cmake/FindDSPONE.cmake
+++ cmake/FindDSPONE.cmake
@@ -4,12 +4,18 @@
 #  DSPONE_INCLUDE_DIRS - The Verbio include directories
 #  DSPONE_LIBRARIES - The libraries needed to use Verbio
 
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_INSTALL_PREFIX})
 
-find_path(DSPONE_INCLUDE_DIR dspone/dsp.h)
-find_library(DSPONE_LIBRARY dspone)
+find_path(GUI_H_DIR dspone/dspgui.h)
+if (GUI_H_DIR)
+    set(DSPONE_GUI, true)
+endif(GUI_H_DIR)
 
-list(APPEND DSPONE_REQUIRED_VARS DSPONE_INCLUDE_DIR)
-list(APPEND DSPONE_REQUIRED_VARS DSPONE_LIBRARY)
+find_path(DSPONE_INCLUDE_DIRS dspone/dsp.h)
+find_library(DSPONE_LIBRARIES dspone)
+
+list(APPEND DSPONE_REQUIRED_VARS DSPONE_INCLUDE_DIRS)
+list(APPEND DSPONE_REQUIRED_VARS DSPONE_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(DSPONE
